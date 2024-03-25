73dcd4cc820447badc61587e7167a1d44fd63c0d
AbhiRawool911 <abhirawool7022@gmail.com>
2024-03-25 12:12:21 +0530

    updated Project card

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,9 @@
-import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer} from './ProjectsStyle'
-import ProjectCard from '../Cards/ProjectCards'
-import { projects } from '../../data/constants'
+import React from 'react';
+import { Container, Wrapper, Title, Desc, CardContainer } from './ProjectsStyle';
+import ProjectCard from '../Cards/ProjectCards';
+import { projects } from '../../data/constants';
 
 const Projects = ({ openModal, setOpenModal }) => {
-  const [toggle, setToggle] = useState('all');
-
   return (
     <Container id="projects">
       <Wrapper>
@@ -15,10 +12,7 @@ const Projects = ({ openModal, setOpenModal }) => {
           I have experience working on various web projects. Below are some examples of the projects I've completed.
         </Desc>
         <CardContainer>
-          {toggle === 'all' && projects.map((project) => (
-            <ProjectCard key={project.id} project={project} openModal={openModal} setOpenModal={setOpenModal} />
-          ))}
-          {projects.filter((item) => item.category === toggle).map((project) => (
+          {projects.map((project) => (
             <ProjectCard key={project.id} project={project} openModal={openModal} setOpenModal={setOpenModal} />
           ))}
         </CardContainer>
