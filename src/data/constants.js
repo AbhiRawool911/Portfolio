c4e59717afa8cd7eb7781db50726da10c4f67fc9
AbhiRawool911 <abhirawool7022@gmail.com>
2024-04-08 02:52:01 +0530

    updated Skills

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,12 +1,11 @@
 export const Bio = {
   name: "Abhishek Rawool",
-  roles: [
-    "Web Developer",
-    "Graphic Designer"
-  ],
-  description: "I am a driven and flexible individual, always seeking out new challenges. With a deep passion for learning, I am dedicated to consistently delivering top-notch results. With a positive mindset and a commitment to growth, I am prepared to make a meaningful contribution and achieve remarkable success.",
+  roles: ["Web Developer", "Graphic Designer"],
+  description:
+    "I am a driven and flexible individual, always seeking out new challenges. With a deep passion for learning, I am dedicated to consistently delivering top-notch results. With a positive mindset and a commitment to growth, I am prepared to make a meaningful contribution and achieve remarkable success.",
   github: "https://github.com/AbhiRawool911",
-  resume: "https://drive.google.com/file/d/1QVIbU7rXPijzaLiMd0rBbJL2CNbZP6DE/view?usp=sharing",
+  resume:
+    "https://drive.google.com/file/d/1OaSh_1fee7mZwAqGIsxFuXwqD8rrdRD9/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhishek-rawool/",
   twitter: "https://twitter.com/Abhi91130",
   insta: "https://www.instagram.com/abhi.rawool_xi._?igsh=MXkwZW5wbzh3dHZrOA==",
@@ -41,9 +40,12 @@ export const skills = [
           "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
       },
       {
+        name: "Redux",
+        image: "https://cdn.worldvectorlogo.com/logos/redux.svg",
+      },
+      {
         name: "Material UI",
-        image:
-          "https://i.postimg.cc/6QPm454w/logo-removebg-preview.png",
+        image: "https://i.postimg.cc/6QPm454w/logo-removebg-preview.png",
       },
     ],
   },
@@ -67,13 +69,16 @@ export const skills = [
       },
       {
         name: "Vercel",
-        image:
-          "https://i.postimg.cc/FHLYbKQv/Screenshot-2024-03-14-195341.png",
+        image: "https://i.postimg.cc/FHLYbKQv/Screenshot-2024-03-14-195341.png",
       },
       {
         name: "VS Code",
         image:
           "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/visual-studio-code-icon.png",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
       },
     ],
   },
@@ -83,19 +88,20 @@ export const experiences = [
   {
     id: 0,
     img: "https://internship.aicte-india.org/uploads/logo/CORPORATE630c6e346d8871661759028.png",
-    role: "Full Stack web Development Intern",
+    role: "Web Development Intern",
     company: "RoborosX Omni Tech Solutions LLP",
-    date: "Feb 2024 - Present",
-    desc: "Engaged as a Full Stack Web Development Intern, tasked with gaining hands-on experience in both front-end and back-end development aspects within web projects.",
+    date: "06 Feb 2024 - 06 Apr 2024",
+    desc: "Engaged as a  Web Development Intern, tasked with gaining hands-on experience in both front-end and back-end development aspects within web projects.",
     skills: [
       "HTML",
       "CSS",
       "JavaScript",
       "Bootstrap",
-      "React Js"
+      "React Js",
+      "Redux",
+      "Firebase",
     ],
-    doc: "https://drive.google.com/file/d/18XKSgU5xJbHI2N4IRtGdH1zkoMdxa-i3/view",
-  }
+  },
 ];
 
 export const education = [
@@ -106,13 +112,14 @@ export const education = [
     date: "Jun 2020 - Jun 2024",
     grade: "6.85 CGPA",
     desc: "I am currently pursuing a Bachelor's degree in Electronics & Communication Engineering at KLS Gogte Institute of Technology, Belgaum.",
-    degree: "Bachelor of Engineering - BE, Electronics & Communication Engineering",
+    degree:
+      "Bachelor of Engineering - BE, Electronics & Communication Engineering",
   },
   {
     id: 1,
     img: "https://i.postimg.cc/1XX3d7xw/Screenshot-2024-03-14-003403.png",
     school: "KLS Gogte PU College of Commerce & Science, Belgaum",
-    date: "Apr 2017 - Apr 2020",
+    date: "Apr 2019 - Apr 2020",
     grade: "58.50%",
     desc: "I completed my class 12 high school education at KLS Gogte PU College of Commerce & Science, Belgaum, where I studied Science with Computer Science.",
     degree: "KSEEB(XII), Science with Computer Science",
@@ -125,7 +132,7 @@ export const education = [
     grade: "8.2 CGPA",
     desc: "I completed my class 10 education at Cantonment Board English Medium School, Belgaum",
     degree: "CBSE(X)",
-  }
+  },
 ];
 
 export const projects = [
@@ -134,8 +141,7 @@ export const projects = [
     title: "Newsie",
     description:
       "Developed the news website using React.js and integrated with the News API, this news website offers a dynamic and user-friendly platform for accessing the latest news across various categories. Leveraging React's component-based architecture, the website ensures seamless navigation and responsiveness across devices. By utilizing the News API, the website fetches news updates, providing users with a diverse range of articles from reputable sources. With an intuitive design and efficient data retrieval, this news website delivers a compelling user experience, keeping visitors informed and engaged with current events.",
-    image:
-      "https://i.postimg.cc/7P5CQtTx/Screenshot-117.png",
+    image: "https://i.postimg.cc/7P5CQtTx/Screenshot-117.png",
     tags: ["React Js"],
     github: "https://github.com/AbhiRawool911/Newsie",
     webapp: "",
@@ -144,15 +150,9 @@ export const projects = [
     id: 1,
     title: "BloggerMe",
     description:
-    "Developed using Bootstrap, HTML, CSS, and Javascript, our basic blogging website offers simplicity and versatility. Enjoy clean design and easy navigation as you explore a platform perfect for sharing your thoughts and stories. With responsive layout and intuitive features, expressing yourself has never been easier. Start sharing your ideas today on our user-friendly blogging platform.",
-    image:
-      "https://i.postimg.cc/C1zj7hg0/Screenshot-121.png",
-    tags: [
-      "HTML",
-      "CSS",
-      "Javascript",
-      "Bootstrap",
-    ],
+      "Developed using Bootstrap, HTML, CSS, and Javascript, our basic blogging website offers simplicity and versatility. Enjoy clean design and easy navigation as you explore a platform perfect for sharing your thoughts and stories. With responsive layout and intuitive features, expressing yourself has never been easier. Start sharing your ideas today on our user-friendly blogging platform.",
+    image: "https://i.postimg.cc/C1zj7hg0/Screenshot-121.png",
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap"],
     github: "https://github.com/AbhiRawool911/BloggerMe",
     webapp: "https://bloggerme.netlify.app/",
   },
@@ -161,8 +161,7 @@ export const projects = [
     title: "Currency Converter",
     description:
       "Developed with HTML, CSS, and Javascript, The Currency Converter website seamlessly integrates with an API to fetch real-time exchange rates. Experience convenience and accuracy as you effortlessly convert currencies. With a user-friendly interface and reliable data retrieval, stay informed and make informed financial decisions hassle-free.",
-    image:
-      "https://i.postimg.cc/KzxYcQ0H/Screenshot-122.png",
+    image: "https://i.postimg.cc/KzxYcQ0H/Screenshot-122.png",
     tags: ["HTML", "CSS", "Javascript"],
     github: "https://github.com/AbhiRawool911/Currency-Converter-",
     webapp: "https://abhir-currency-converter.vercel.app/",
