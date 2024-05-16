e18d6797edf97c04f042121eba3230ffa54941c9
AbhiRawool911 <abhirawool7022@gmail.com>
2024-05-16 16:50:05 +0530

    Updated project

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a driven and flexible individual, always seeking out new challenges. With a deep passion for learning, I am dedicated to consistently delivering top-notch results. With a positive mindset and a commitment to growth, I am prepared to make a meaningful contribution and achieve remarkable success.",
   github: "https://github.com/AbhiRawool911",
   resume:
-    "https://drive.google.com/file/d/1OaSh_1fee7mZwAqGIsxFuXwqD8rrdRD9/view?usp=sharing",
+    "https://drive.google.com/file/d/1TXygf40hwraZfjMfPPC6HJTRGs3dqo0D/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhishek-rawool/",
   twitter: "https://twitter.com/Abhi91130",
   insta: "https://www.instagram.com/abhi.rawool_xi._?igsh=MXkwZW5wbzh3dHZrOA==",
@@ -35,6 +35,11 @@ export const skills = [
           "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
       },
       {
+        name: "Tailwind CSS",
+        image:
+          "https://imgs.search.brave.com/EKsk4xtJu-qcCLu6v_GJEiJbWXakB9PzJp_snmXg4ak/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy90YWlsd2luZC1j/c3MtMi5zdmc.svg",
+      },
+      {
         name: "React Js",
         image:
           "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
@@ -46,6 +51,30 @@ export const skills = [
       {
         name: "Material UI",
         image: "https://i.postimg.cc/6QPm454w/logo-removebg-preview.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image:
+          "https://imgs.search.brave.com/tq3hucM-dRx8cLV-5lQQ5Qy22SzDqFN1foHTH9A2ZL8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL04vbm9kZWpz/LWxvZ28tRkJFMTIy/RTM3Ny1zZWVrbG9n/by5jb20ucG5n",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://imgs.search.brave.com/dYOC8zWGw9HfWegfPLqE6kjIzG2RgPSySVu8jae78Ss/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Z3VheWVyZC5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMjEv/MDQvZXhwcmVzc2pz/LWxvZ28uc3Zn.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://imgs.search.brave.com/PAakdg_TB0vy-41VFjxaXnAXMlv0tJMbaCKwZa7K_UA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9yYXcu/Z2l0aHVidXNlcmNv/bnRlbnQuY29tL2Rl/dmljb25zL2Rldmlj/b24vbWFzdGVyL2lj/b25zL21vbmdvZGIv/bW9uZ29kYi1vcmln/aW5hbC13b3JkbWFy/ay5zdmc.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
       },
     ],
   },
@@ -63,22 +92,9 @@ export const skills = [
           "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
       },
       {
-        name: "Netlify",
-        image:
-          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
-      },
-      {
-        name: "Vercel",
-        image: "https://i.postimg.cc/FHLYbKQv/Screenshot-2024-03-14-195341.png",
-      },
-      {
         name: "VS Code",
         image:
           "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/visual-studio-code-icon.png",
-      },
-      {
-        name: "Firebase",
-        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
       },
     ],
   },
@@ -91,7 +107,7 @@ export const experiences = [
     role: "Web Development Intern",
     company: "RoborosX Omni Tech Solutions LLP",
     date: "06 Feb 2024 - 06 Apr 2024",
-    desc: "Engaged as a  Web Development Intern, tasked with gaining hands-on experience in both front-end and back-end development aspects within web projects.",
+    desc: "Demonstrated exceptional dedication and professionalism, accounting for 50% of the evaluation. Successfully completed the Development of Computer Based Test Feature for RosX Edu Soft, contributing significantly to 40% of the tasks. Showed a remarkable willingness to learn and improve, representing 10% of overall achievements.",
     skills: [
       "HTML",
       "CSS",
@@ -137,31 +153,41 @@ export const education = [
 
 export const projects = [
   {
+    id: 3,
+    title: "BlogSync",
+    description:
+      "BlogSync is a MERN blogging platform that caters to bloggers of all levels, offering a seamless experience for creating, sharing, and managing content. Its modern Blog Editor powered by Editor JS provides a user-friendly environment, while Google Authentication ensures secure login. Dynamic URLs simplify sharing, and a robust Search Page facilitates easy discovery. Users enjoy dedicated profiles, a Dashboard for efficient management, detailed Blog Post Analytics, interactive features like likes and comments, real-time Notifications, and profile customization. With mobile responsiveness and a modern design, BlogSync elevates the blogging experience for users across devices, making it a go-to platform for fostering community and engagement.",
+    image:
+      "https://github.com/AbhiRawool911/Newsie/assets/157002044/9e640c07-899c-4cd7-a16d-f6a22d37a491",
+    tags: [
+      "Tailwind CSS",
+      "MongoDB",
+      "Express Js",
+      "React Js",
+      "Node Js",
+      "Firebase",
+    ],
+    github: "https://github.com/AbhiRawool911/BlogSync-MERN-Blogging-Website",
+    webapp: "https://blogsync.netlify.app",
+  },
+  {
     id: 0,
     title: "Newsie",
     description:
       "Developed the news website using React.js and integrated with the News API, this news website offers a dynamic and user-friendly platform for accessing the latest news across various categories. Leveraging React's component-based architecture, the website ensures seamless navigation and responsiveness across devices. By utilizing the News API, the website fetches news updates, providing users with a diverse range of articles from reputable sources. With an intuitive design and efficient data retrieval, this news website delivers a compelling user experience, keeping visitors informed and engaged with current events.",
-    image: "https://i.postimg.cc/7P5CQtTx/Screenshot-117.png",
-    tags: ["React Js"],
+    image:
+      "https://github.com/AbhiRawool911/Newsie/assets/157002044/7f4fb51f-1a73-4dd4-a4ad-afa6606aa163",
+    tags: ["React Js", "News API"],
     github: "https://github.com/AbhiRawool911/Newsie",
     webapp: "",
   },
   {
     id: 1,
-    title: "BloggerMe",
-    description:
-      "Developed using Bootstrap, HTML, CSS, and Javascript, our basic blogging website offers simplicity and versatility. Enjoy clean design and easy navigation as you explore a platform perfect for sharing your thoughts and stories. With responsive layout and intuitive features, expressing yourself has never been easier. Start sharing your ideas today on our user-friendly blogging platform.",
-    image: "https://i.postimg.cc/C1zj7hg0/Screenshot-121.png",
-    tags: ["HTML", "CSS", "Javascript", "Bootstrap"],
-    github: "https://github.com/AbhiRawool911/BloggerMe",
-    webapp: "https://bloggerme.netlify.app/",
-  },
-  {
-    id: 2,
     title: "Currency Converter",
     description:
       "Developed with HTML, CSS, and Javascript, The Currency Converter website seamlessly integrates with an API to fetch real-time exchange rates. Experience convenience and accuracy as you effortlessly convert currencies. With a user-friendly interface and reliable data retrieval, stay informed and make informed financial decisions hassle-free.",
-    image: "https://i.postimg.cc/KzxYcQ0H/Screenshot-122.png",
+    image:
+      "https://github.com/AbhiRawool911/Currency-Converter/assets/157002044/28a8a879-a4ce-49ec-ba7c-984ad81ae845",
     tags: ["HTML", "CSS", "Javascript"],
     github: "https://github.com/AbhiRawool911/Currency-Converter-",
     webapp: "https://abhir-currency-converter.vercel.app/",
